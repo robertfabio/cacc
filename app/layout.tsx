fd1a27680a69221299808fd114c819df36565cf8
Fábio Gurgel <fabimoficialcontas@gmail.com>
2025-04-07 17:35:16 -0300

    Adicionado Vercel Analytics para monitoramento de uso

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter, Montserrat } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="pt-BR" className={`${inter.variable} ${montserrat.variable}`}>
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
